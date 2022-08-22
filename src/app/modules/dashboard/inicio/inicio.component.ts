1a97884dedd8f5178dccd53a47f45c7582ed4946
cristiam <cristiamvasquez@gmail.com>
2022-08-21 22:57:18 -0400

    protoio inicio pseudofuncional

--- src/app/modules/dashboard/inicio/inicio.component.ts
+++ src/app/modules/dashboard/inicio/inicio.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  categories = ["Diseno", "FrontEnd", "BackEnd", "Arquitectura", "Liderazgo"];
+  result = {
+    "diseño": "hades",
+    "liderazgo": "hades",
+    "backend": "hercules",
+    "arquitectura": "hercules",
+    "frontend": "zeus"
+  };
+  levels:any =
+    {
+      hades: [
+        { level: ['lock', 'lock', 'unlock', 'lock', 'lock'] },
+        { level: ['lock', 'lock', 'lock'] },
+        { level: ['lock', 'lock', 'lock'] },
+        { level: ['lock'] }],
+      hercules: [
+        { level: ['lock', 'unlock', 'unlock', 'unlock', 'lock'] },
+        { level: ['lock', 'lock', 'lock'] },
+        { level: ['lock', 'lock', 'lock', 'unlock'] },
+        { level: ['lock'] }],
+      zeus: [
+        { level: ['unlock', 'lock', 'unlock', 'lock', 'unlock'] },
+        { level: ['lock', 'lock', 'lock'] },
+        { level: ['lock', 'lock', 'lock'] },
+        { level: ['unlock'] }]
+    }
+
+
+
+
   constructor() { }
 
   ngOnInit(): void {
